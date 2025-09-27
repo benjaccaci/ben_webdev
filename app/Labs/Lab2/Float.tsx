2ca0a19d129167e4fd7e4d9ce58e56e37ea7c1bd
ben_jaccaci <ben.jaccaci@gmail.com>
2025-09-27 12:18:23 -0400

    Adding React Bootstrap

--- app/Labs/Lab2/Float.tsx
+++ app/Labs/Lab2/Float.tsx
@@ -23,6 +23,10 @@ export default function Float() {
         >
           Red{" "}
         </div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
         <div className="wd-float-done"></div>
       </div>
     </div>
