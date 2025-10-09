26d87801053e532347543683fab1b080d6948e63
ben_jaccaci <ben.jaccaci@gmail.com>
2025-10-08 23:18:54 -0400

    L3 Exercises

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -1,8 +1,38 @@
-function Lab3() {
- return (
-  <div>
-   <h2>Lab 3</h2>
-  </div>
- );
+import BooleanVariables from "./BooleanVariables";
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import IfElse from "./IfElse";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FilterFunction from "./FilterFunction";
+
+export default function Lab3() {
+  return (
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <ImpliedReturn />
+      <TemplateLiterals />
+      <SimpleArrays />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FilterFunction />
+    </div>
+  );
 }
-export default Lab3;
