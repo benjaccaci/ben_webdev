0aeba66dd7c7153e4f96fffd05c28c34d31808d5
ben_jaccaci <ben.jaccaci@gmail.com>
2025-10-17 19:02:34 -0400

    working on A3

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -14,7 +14,21 @@ import MapFunction from "./MapFunction";
 import FindFunction from "./FindFunction";
 import FilterFunction from "./FilterFunction";
 import House from "./House";
-
+import JsonStringify from "./JsonStringify";
+import TodoList from "./todos/TodoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import Add from "./Add";
+import Square from "./Square";
+import Highlight from "./Highlight";
+import PathParameters from "./PathParameters";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
 export default function Lab3() {
   return (
     <div id="wd-lab3">
@@ -23,6 +37,10 @@ export default function Lab3() {
       <VariableTypes />
       <BooleanVariables />
       <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <hr />
       <LegacyFunctions />
       <ArrowFunctions />
       <ImpliedReturn />
@@ -35,6 +53,26 @@ export default function Lab3() {
       <FindFunction />
       <FilterFunction />
       <House />
+      <JsonStringify />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
+      <Classes />
+      <Styles />
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square> 4 </Square>
+      <hr />
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione
+        eaque illo minus cum, saepe totam vel nihil repellat nemo explicabo
+        excepturi consectetur. Modi omnis minus sequi maiores, provident
+        voluptates.
+      </Highlight>
+      <hr />
+      <PathParameters />
     </div>
   );
 }
