72fa6c9c6974e2494f3a6e67545e977567411691
ben_jaccaci <ben.jaccaci@gmail.com>
2025-10-17 19:41:49 -0400

    Working on A3 more

--- app/(Kambaz)/navigation.tsx
+++ app/(Kambaz)/navigation.tsx
@@ -1,109 +1,53 @@
-import { AiFillBook, AiOutlineBook, AiOutlineDashboard } from "react-icons/ai";
+"use client";
+import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
-import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
+import { LiaBookSolid } from "react-icons/lia";
 import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 import Link from "next/link";
-import { BiBook, BiHelpCircle, BiInfoCircle } from "react-icons/bi";
+import { usePathname } from "next/navigation";
 import { CiBeaker1 } from "react-icons/ci";
 export default function KambazNavigation() {
+  const pathname = usePathname();
+  const links = [
+    { href: "/Account", label: "Account", icon: FaRegCircleUser },
+    { href: "/Dashboard", label: "Dashboard", icon: AiOutlineDashboard },
+    { href: "/Dashboard", label: "Courses", icon: LiaBookSolid },
+    { href: "/Calendar", label: "Calendar", icon: IoCalendarOutline },
+    { href: "/Inbox", label: "Inbox", icon: FaInbox },
+    { href: "/Labs", label: "Labs", icon: CiBeaker1 },
+  ];
+
   return (
     <ListGroup
-      className="rounded-0 position-fixed
-                    bottom-0
-                    top-0 d-none d-md-block
-                    bg-black z-2"
+      id="wd-kambaz-navigation"
       style={{ width: 120 }}
+      className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
     >
       <ListGroupItem
-        className="bg-black border-0 text-center"
-        as="a"
+        id="wd-neu-link"
         target="_blank"
         href="https://www.northeastern.edu/"
+        action
+        className="bg-black border-0 text-center"
       >
-        <img src="/images/NEU.png" width="75px" alt="Northeastern University" />
+        <img src="/images/NEU.png" width="75px" />
       </ListGroupItem>
-      <ListGroupItem className="border-0 bg-black text-center">
-        <Link
-          href="/Account"
-          id="wd-account-link"
-          className="text-white text-decoration-none"
+      {links.map(({ href, label, icon: Icon }) => (
+        <ListGroupItem
+          key={href}
+          as={Link}
+          href={href}
+          className={`${
+            pathname.includes(label)
+              ? "bg-white text-danger"
+              : "bg-black text-white"
+          } text-center border-0`}
         >
-          <FaRegCircleUser className="fs-1 text-white" />
-          <br />
-          Account{" "}
-        </Link>
-      </ListGroupItem>
-      <ListGroupItem
-        className="border-0
-                  bg-white text-center"
-      >
-        <Link
-          href="/Dashboard"
-          className="text-danger
-           text-decoration-none"
-        >
-          <AiOutlineDashboard className="fs-1 text-danger" />
-          <br />
-          Dashboard
-        </Link>
-      </ListGroupItem>
-      <ListGroupItem
-        className="border-0
-                  bg-black text-center"
-      >
-        <Link
-          href="/Dashboard"
-          className="text-danger
-           text-decoration-none"
-        >
-          <BiBook className="fs-1 text-danger" />
-          <br />
-          Courses
-        </Link>
-      </ListGroupItem>
-      <ListGroupItem
-        className="border-0
-                  bg-black text-center"
-      >
-        <Link
-          href="/Help"
-          className="text-danger
-           text-decoration-none"
-        >
-          <BiHelpCircle className="fs-1 text-danger" />
-          <br />
-          Help
-        </Link>
-      </ListGroupItem>
-      <ListGroupItem
-        className="border-0
-                  bg-black text-center"
-      >
-        <Link
-          href="/Info"
-          className="text-danger
-           text-decoration-none"
-        >
-          <BiInfoCircle className="fs-1 text-danger" />
-          <br />
-          Info
-        </Link>
-      </ListGroupItem>
-      <ListGroupItem
-        className="border-0
-                  bg-black text-center"
-      >
-        <Link
-          href="/Labs"
-          className="text-danger
-           text-decoration-none"
-        >
-          <CiBeaker1 className="fs-1 text-danger" />
-          <br />
-          Labs
-        </Link>
-      </ListGroupItem>
+          <Icon className="fs-1 text-danger" /> <br />
+          {label}
+        </ListGroupItem>
+      ))}
     </ListGroup>
   );
 }
