0cfcca8282a2fe6e130f32808aaad9c78a5e7ec8
ben_jaccaci <ben.jaccaci@gmail.com>
2025-10-28 20:38:58 -0400

    Working on Lab4

--- app/Labs/Lab4/page.tsx
+++ app/Labs/Lab4/page.tsx
@@ -9,25 +9,37 @@ import DateStateVariable from "./DateStateVariable";
 import ObjectStateVariable from "./ObjectStateVariable";
 import ArrayStateVariable from "./ArrayStateVariable";
 import ParentStateComponent from "./ParentStateComponent";
+import React from "react";
+import ReduxExamples from "./ReduxExamples/page";
+import store from "./store";
+import { Provider } from "react-redux";
+import HelloRedux from "./ReduxExamples/HelloRedux/page";
+import CounterRedux from "./ReduxExamples/CounterRedux/page";
+import EventObject from "./EventObject";
 
 export default function Lab4() {
   function sayHello() {
     alert("Hello");
   }
-
   return (
-    <div id="wd-passing-functions">
-      <h3>Lab 4</h3>
-      <HandlingClickEvent />
-      <PassingDataOnEvent />
-      <PassingFunctions someFunction={sayHello} />
-      <Counter />
-      <BooleanStateVariables />
-      <StringStateVariables />
-      <DateStateVariable />
-      <ObjectStateVariable />
-      <ArrayStateVariable />
-      <ParentStateComponent />
-    </div>
+    <Provider store={store}>
+      <div id="wd-passing-functions">
+        <h3>Lab 4</h3>
+        <HandlingClickEvent />
+        <PassingDataOnEvent />
+        <PassingFunctions someFunction={sayHello} />
+        <EventObject />
+        <Counter />
+        <BooleanStateVariables />
+        <StringStateVariables />
+        <DateStateVariable />
+        <ObjectStateVariable />
+        <ArrayStateVariable />
+        <ParentStateComponent />
+        <ReduxExamples />
+        <HelloRedux />
+        <CounterRedux />
+      </div>
+    </Provider>
   );
 }
