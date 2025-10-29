f58f12bbc4ae0698b4ad78356e13922f5104d788
ben_jaccaci <ben.jaccaci@gmail.com>
2025-10-28 21:10:16 -0400

    Fixing client issues

--- app/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
+++ app/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
 import { useState } from "react";
 import { Button, FormControl, ListGroup, ListGroupItem } from "react-bootstrap";
 import TodoForm from "./TodoForm";
